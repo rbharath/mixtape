Fixed some numerical issues in Q-solving

Mixtape/mslds_solver.py
@@ -23,7 +23,7 @@ class MetastableSwitchingLDSSolver(object):
     def do_hmm_mstep(self, stats):
         print "Starting hmm mstep"
         print "Starting transmat update"
-        transmat = transmat_solve(stats)
+        transmat = self.transmat_solve(stats)
         print "Starting means update"
         means = self.means_update(stats)
         print "Starting covars update"
@@ -39,7 +39,7 @@ class MetastableSwitchingLDSSolver(object):
         As = np.copy(As)
         Qs = np.copy(Qs)
         bs = np.copy(bs)
-        transmat = transmat_solve(stats)
+        transmat = self.transmat_solve(stats)
         A_upds, Q_upds, b_upds = self.AQb_update(As, Qs, bs,
                 means, covars, stats, N_iter=N_iter, verbose=verbose,
                 gamma=gamma, tol=tol, num_biconvex=num_biconvex)
@@ -108,8 +108,7 @@ class MetastableSwitchingLDSSolver(object):
 
     def AQb_update(self, As, Qs, bs, means, covars, stats, N_iter=400,
                     verbose=False, gamma=.5, tol=1e-1, num_biconvex=2):
-        Bs, Cs, Es, Ds, Fs = compute_aux_matrices(self.n_components,
-                self.n_features, As, bs, covars, stats)
+        Bs, Cs, Es, Ds, Fs = self.compute_aux_matrices(As, bs, covars, stats)
         self.print_aux_matrices(Bs, Cs, Es, Ds, Fs)
         A_upds, Q_upds, b_upds = [], [], []
 
@@ -152,7 +151,7 @@ class MetastableSwitchingLDSSolver(object):
         return b
 
     # FIX ME!
-    def transmat_solve(stats):
+    def transmat_solve(self, stats):
         counts = (np.maximum(stats['trans'], 1e-20).astype(np.float64))
         # Need to fix this......
         #self.transmat_, self.populations_ = \

Mixtape/mslds_solvers/Q_problems.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.linalg import LinAlgError
 from mixtape.utils import print_solve_test_case
 from mixtape.mslds_solvers.sparse_sdp.utils import get_entries, set_entries
 from mixtape.mslds_solvers.sparse_sdp.constraints import many_batch_equals
@@ -26,16 +27,12 @@ class Q_problem(object):
         np.seterr(divide='raise')
         (D_ADA_T_cds, I_1_cds, I_2_cds, R_cds) = self.coords()
         R = get_entries(X, R_cds)
-        # Need to avoid ill-conditioning of R
-        R = R + (1e-5) * np.eye(self.dim)
         try:
             L = np.linalg.cholesky(R)
             log_det = 2*np.sum(np.log(np.diag(L)))
             val = -log_det + np.trace(np.dot(R, B))
-        except FloatingPointError:
-            if ((np.linalg.det(R) < np.finfo(np.float).eps)
-                or not np.isfinite(np.linalg.det(R))):
-                val = np.inf
+        except (FloatingPointError, LinAlgError) as e:
+            val = np.inf
         return val 
 
     # grad - log det R = -R^{-1} = -Q (see Boyd and Vandenberge, A4.1)
@@ -205,6 +202,8 @@ class Q_problem(object):
             Q = np.linalg.inv(R)
             # Unscale answer
             Q *= (1./scale)
+            import pdb
+            pdb.set_trace()
             return Q
 
     def print_Q_test_case(test_file, A, D, F, dim):

Mixtape/utils.py
@@ -27,6 +27,7 @@ import numpy as np
 from sklearn.utils import check_random_state
 from sklearn.externals.joblib import load, dump
 from numpy.linalg import norm
+import pickle
 
 #-----------------------------------------------------------------------------
 # Code
@@ -119,16 +120,16 @@ def print_solve_test_case(name, matrices, dim, test_file):
         disp += "\timport pickle\n"
         disp += "\tfrom mixtape.mslds_solver import AQb_solve,"\
                             + " A_solve, Q_solve\n"
-        test_string += "\tblock_dim = %d\n"%dim
+        disp += "\tblock_dim = %d\n"%dim
         arg_string = ""
         for mat, mat_name in matrices:
             pickle.dump(mat, open("%s_%s_test.p" % (mat_name, name), "w"))
             test_string += ('\t%s = pickle.load(open("%s_%s_test.p", "r"))\n'
                             % mat_name, mat_name, name)
             arg_string += mat_name + ", "
-        test_string += "\t%s_solve(block_dim, %s\n" % name, arg_string
-        test_string += "\t\tdisp=True, debug=False, verbose=False)\n"
-        f.write(test_string)
+        disp += "\t%s_solve(block_dim, %s\n" % name, arg_string
+        disp += "\t\tdisp=True, debug=False, verbose=False)\n"
+        f.write(disp)
     np.set_printoptions(threshold=1000)
 
 